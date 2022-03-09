289d13dc167e7b0fe697eb35a6fd2859f9f81c59
Yaros5 <yaroslav_9@icloud.com>
2022-03-09 20:34:03 +0100

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,24 +1,24 @@
 res = 0
 puts "Enter_number"
-enter_number_1 = gets
+enter_number_1 = gets.chomp()
 puts "Enter_number"
-enter_number_2 = gets
+enter_number_2 = gets.chomp()
 puts "Enter * - / +"
-enter_number_3 = gets
+enter_number_3 = gets.chomp()
 if enter_number_3 == "+"
     res = enter_number_1.to_i + enter_number_2.to_i
     print("Result is: #{res} \n")
 end
 if enter_number_3 == "-"
-    res = enter_number_1.to_i + enter_number_2.to_i
+    res = enter_number_1.to_i - enter_number_2.to_i
     print("Result is: #{res} \n")
 
 end
 if enter_number_3 == "*"
-    res = enter_number_1.to_i + enter_number_2.to_i
+    res = enter_number_1.to_i * enter_number_2.to_i
     print("Result is: #{res} \n")
 end
 if enter_number_3 == "/"
-    res = enter_number_1.to_f + enter_number_2.to_f
+    res = enter_number_1.to_f / enter_number_2.to_f
     print("Result is: #{res} \n")
 end
